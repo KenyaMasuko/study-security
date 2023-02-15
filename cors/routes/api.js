9010d812359fbcebff81eaff77fa48a69f3e0174
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-02-16 08:33:26 +0900

    クロスオリジンからのリクエストを許可

--- cors/routes/api.js
+++ cors/routes/api.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.setHeader("X-Timestamp", Date.now());
-	let message = req.query.message;
-	const lang = req.headers["x-lang"];
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
 
-	if (message === "") {
-		res.status(400);
-		if (lang === "en") {
-			message = "message is empty";
-		} else {
-			message = "messageの値が空です";
-		}
-	}
-	res.send({ message });
+router.get("/", (req, res) => {
+  res.setHeader("X-Timestamp", Date.now());
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
+
+  if (message === "") {
+    res.status(400);
+    if (lang === "en") {
+      message = "message is empty";
+    } else {
+      message = "messageの値が空です";
+    }
+  }
+  res.send({ message });
 });
 
 router.use(express.json());
 router.post("/", (req, res) => {
-	const body = req.body;
-	console.log(body);
-	res.end();
+  const body = req.body;
+  console.log(body);
+  res.end();
 });
 
 module.exports = router;
