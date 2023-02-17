3edb51007b25ef893d2000f8add4a0a7f535fe06
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-02-18 08:11:04 +0900

    feat: csp対策をなしでjsを実行

---
+++ csp/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const api = require("./routes/api");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use("/api", api);
+
+app.get("/", (req, res, next) => {
+	res.end("Top Page");
+});
+
+app.get("/csp", (req, res) => {
+	res.render("csp");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
